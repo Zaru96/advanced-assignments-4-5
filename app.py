0246f9824007ca0b501fd9921533a086e12e64fe
Addinul Haq <140295639+Zaru96@users.noreply.github.com>
2026-05-11 12:02:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,27 +6,29 @@ import joblib
 model = joblib.load('model.joblib')
 
 st.title("Titanic Survival Prediction")
-st.write("Prediksi apakah penumpang Titanic akan selamat atau tidak.")
+st.write("Prediksi keselamatan penumpang Titanic")
 
 # Input user
 pclass = st.selectbox("Passenger Class", [1, 2, 3])
+
 sex = st.selectbox("Sex", ["male", "female"])
-age = st.slider("Age", 1, 80, 25)
+
 sibsp = st.number_input("Siblings/Spouses Aboard", 0, 10, 0)
+
 parch = st.number_input("Parents/Children Aboard", 0, 10, 0)
+
 fare = st.number_input("Fare", 0.0, 600.0, 50.0)
 
-# Encoding
-sex = 1 if sex == "male" else 0
+embarked = st.selectbox("Embarked", ["S", "C", "Q"])
 
 # Dataframe input
 input_data = pd.DataFrame({
     'Pclass': [pclass],
     'Sex': [sex],
-    'Age': [age],
     'SibSp': [sibsp],
     'Parch': [parch],
-    'Fare': [fare]
+    'Fare': [fare],
+    'Embarked': [embarked]
 })
 
 # Prediction
